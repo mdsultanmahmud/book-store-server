a643b83c43d856bc949fe602634e5892841f8c7d
mdsultanmahmud <mdsultanmahmud.bd00@gmail.com>
2023-05-27 19:04:22 +0600

    update and delete product api done

--- controllers/book.controller.js
+++ controllers/book.controller.js
@@ -1,10 +1,11 @@
+const { ObjectId } = require("mongodb")
 const { getDb } = require("../utils/dbConnect")
 
 
 module.exports.getAllBook = async (req, res) => {
     try {
         const db = getDb()
-        const result = await db.collection("users").find({}).toArray()
+        const result = await db.collection("bookList").find({}).toArray()
         res.send(result)
     } catch (error) {
         console.log(error)
@@ -18,6 +19,38 @@ module.exports.addABook = async (req, res) => {
         const result = await db.collection("bookList").insertOne(book)
         res.status(200).json({
             message: "Book Added Successfully",
+            data: result 
+        })
+    } catch (error) {
+        console.log(error)
+    }
+}
+module.exports.updateABook = async (req, res) => {
+    try {
+        const db = getDb()
+        const id = req.params.id
+        const result = await db
+        .collection("bookList")
+        .updateOne({_id: new ObjectId(id)}, {$set: req.body})
+
+        res.status(200).json({
+            message: `Book Updated Successfully with ${id} book id`,
+            data: result 
+        })
+    } catch (error) {
+        console.log(error)
+    }
+}
+module.exports.deleteABook = async (req, res) => {
+    try {
+        const db = getDb()
+        const id = req.params.id
+        const result = await db
+        .collection("bookList")
+        .deleteOne({_id: new ObjectId(id)})
+
+        res.status(200).json({
+            message: `Book Deleted Successfully with ${id} book id`,
             data: result 
         })
     } catch (error) {
--- routes/v1/book.route.js
+++ routes/v1/book.route.js
@@ -5,4 +5,8 @@ router.route("/")
    .get(booksControllers.getAllBook)
    .post(booksControllers.addABook)
 
+router.route("/:id")
+    .patch(booksControllers.updateABook)
+    .delete(booksControllers.deleteABook)
+
 module.exports = router
